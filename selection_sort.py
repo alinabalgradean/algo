1f375a2e6f8eb7095f94fa662c3c56abf5eaeb56
alinabalgradean <32243667+alinabalgradean@users.noreply.github.com>
2018-08-19 21:21:47 +0200

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,21 @@
+def MySelectionSort(lst):
+  """Unoptimized version of the bubble sort algorithm.
+  
+  Args:
+    lst [list]: The list to be sorted.
+    
+  Returns:
+    lst [list]: Sorted list.
+  """
+  n = len(lst)
+  
+  for i in range(n):
+    
+    wall = i
+    for j in range(i+1, n):
+      if lst[wall] > lst[j]:
+        wall = j
+        
+  lst[wall], lst[i] = lst[i], lst[wall]
+        
+  return lst
